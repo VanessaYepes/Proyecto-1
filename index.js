9919783a8c2f6bc6a8fe4467ac6fb4644ae214fd
VanessaYepes <vanessa.yepesg@udea.edu.co>
2022-11-08 22:09:07 -0500

    creacion nuevos metodos get con parametros y sin parametos

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log("hola mundo");
+//const express = require('express')  este manera antigua de importacion commonjs
+import Express from 'express' //manera nueva es6 --> usar esta
+const app = Express()
+const port = 3000
+
+app.get('/', (req, res) => { // una / es el nativo "/ " -> direccion a la que apunto
+  res.send('Hello World!')
+})
+
+app.get("/vane/:id",(req,res)=>{ //res--> respuesta o salida , req--> entrada 
+    console.log(req.params)
+    res.send('Hola desde vane '+ req.params.id)
+})
+
+app.get("/vane",(req,res)=>{ //res--> respuesta o salida , req--> entrada 
+    res.send('Hola desde vane ')
+})
+
+app.listen(port, () => { //para levantar el servidor 
+  console.log(`Example app listening on port ${port}`)
+})
